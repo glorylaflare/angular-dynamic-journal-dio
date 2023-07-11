29ad6b44a0291aad51abe900f9fdfe9ea7ab4029
Marcelo Gonçalves <juninho_toh@hotmail.com>
2023-07-11 15:41:07 -0300

    reorganizando layout e deixando-o inteligente

--- src/app/components/stories/stories.component.ts
+++ src/app/components/stories/stories.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stories',
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stories.component.css']
 })
 export class StoriesComponent implements OnInit {
+  @Input()
+  cover:string = "";
+  @Input()
+  title:string = "";
+  @Input()
+  description:string= "";
+  @Input()
+  authorPhoto:string= "";
+  @Input()
+  authorName:string= "";
+  @Input()
+  date:string= "";
 
   constructor() { }
 
